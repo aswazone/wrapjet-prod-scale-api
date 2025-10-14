62e9489f9840fb2c5595637b1e7dd5408328970e
Aswin <itsme.aswinkp@gmail.com>
2025-10-14 20:30:37 +0530

    Configure Eslint and Prettier !

--- src/app.js
+++ src/app.js
@@ -3,7 +3,7 @@ import express from 'express';
 const app = express();
 
 app.get('/', (req, res) => {
-    res.status(200).send('Hello from WrapJet-Prod-Scale-API ✨');
+  res.status(200).send('Hello from WrapJet-Prod-Scale-API ✨');
 });
 
 export default app;
