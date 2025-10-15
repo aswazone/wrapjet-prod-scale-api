98a21c07656b489fd391d3335f4b0ea87f682ade
Aswin <itsme.aswinkp@gmail.com>
2025-10-15 17:02:17 +0530

    Configured Neon-postgresql database w/ drizzle-orm !

--- src/app.js
+++ src/app.js
@@ -1,8 +1,10 @@
 import express from 'express';
+import 'dotenv/config';
 
 const app = express();
 
 app.get('/', (req, res) => {
+  console.log(process.env.DATABASE_URL,'DATABASE_URL');
   res.status(200).send('Hello from WrapJet-Prod-Scale-API ✨');
 });
 
